4b4183cfba05521d33af7aaa2d633d979de41f70
Yannis Piot Pilot <yannis@piotpilot.eu>
2018-03-27 15:55:37 +0200

    Exo 3 ? soon :D

--- exercice02.js
+++ exercice02.js
@@ -1,22 +1,27 @@
 function exercice02(){
-var message = "bonjour à tous!";
-var longueur = message.length;
-var i = longueur;
-var espace = "";
-var o = "";
-var indexO = "";
-for (i = 0 ; i < longueur; i++){
-    if(message[i] ===  " "){
-        espace++;
-       
-    }
-    if(message[i] ===  "o"){
-        o++;
-        indexO += i + ".";
-    }
-}
+        var message = "bonjour a tous!";
+        var longueur = message.length;
+        var i = longueur;
 
-    console.log("il  y a : " + espace + " espaces");
-    console.log("il y a : " + o + " o");
-    console.log(indexO);
+        //Déclare les variables a vide.
+        var space = "";
+        var o = "";
+        var indexO = "";
+
+        for (i = 0 ; i < longueur; i++){
+            //test les espace
+            if(message[i] ===  " "){
+                space++;
+            
+            }
+            //test les o
+            if(message[i] ===  "o"){
+                o++;
+                indexO += i + ".";
+            }
+        }
+
+            console.log("il y a : " + space + " espaces");
+            console.log("il y a : " + o + " o");
+            console.log(indexO);
 }
